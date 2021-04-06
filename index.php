15e3601ed0949aea85d0974404c6121d94a542f9
Max-Sheehe <mms0079@mix.wvu.edu>
2021-04-06 15:51:14 -0400

    Minor changes to about us page. Everyone ought to add their tile. see comment in index.php

--- index.php
+++ index.php
@@ -4,58 +4,57 @@ require 'includes/header.php';
 
 <main>
 
-	<link rel="stylesheet" href="css/about.css">
+    <link rel="stylesheet" href="css/about.css">
 
-	<div class="about">
-		<h1>About Us</h1>
-		<p>a;dfjieoanvnkcnafkjdfjkabdjfb</p>
-	</div>
+    <div class="about">
+        <h1>Our Team</h1>
+    </div>
 
-	<div class="row">
-		<div class="column">
-			<div class="card">
-				<img src="images/dogecoin.jpg">
-				<div class="container">
-					<h2>CEO</h2>
-					<p>Harrison Ford</p>
-					<p>Email: solo@cs230.com</p>
-				</div>
-			</div>
-		</div>
-
-		<div class="column">
-                        <div class="card">
-                                <img src="images/doge.jpg">
-                                <div class="container">
-                                        <h2>CEO</h2>
-                                        <p>Harrison Ford</p>
-                                        <p>Email: solo@cs230.com</p>
-                                </div>
-                        </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/About_Us_Max_Sheehe.jpg" class="center"> 
+                <!--make sure to use the center class to center your profile image-->
+                <div class="container">
+                    <h2>Max Sheehe</h2>
+                    <p>Dog's Name: Bear</p>
+                    <p>Email: mms0079@mix.wvu.edu</p>
                 </div>
+            </div>
+        </div>
 
-		<div class="column">
-                        <div class="card">
-                                <img src="images/doge-moon.jpg">
-                                <div class="container">
-                                        <h2>CEO</h2>
-                                        <p>Harrison Ford</p>
-                                        <p>Email: solo@cs230.com</p>
-                                </div>
-                        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/doge.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
                 </div>
+            </div>
+        </div>
 
-		<div class="column">
-                        <div class="card">
-                                <img src="images/doge-ram.jpg">
-                                <div class="container">
-                                        <h2>CEO</h2>
-                                        <p>Harrison Ford</p>
-                                        <p>Email: solo@cs230.com</p>
-                                </div>
-                        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/doge-moon.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
                 </div>
-	</div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/doge-ram.jpg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Harrison Ford</p>
+                    <p>Email: solo@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </main>
-
