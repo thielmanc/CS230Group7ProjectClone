a5da10793eddf107d5f3f3e394ca2927382aaacd
arw6329 <arw0048@mix.wvu.edu>
2021-04-08 13:16:52 -0400

    Minor cleanup and error fixes across several files

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,6 @@
-<?php
-session_start();
-?>
+<?php session_start() ?>
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -13,17 +10,9 @@ session_start();
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.js"
-        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/0809ee8fa6.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
     <script src="js/header.js"></script>
 </head>
 <header>
@@ -36,54 +25,35 @@ session_start();
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-
-                <?php if (isset($_SESSION['uid'])){
-                    if ($_SESSION['privileged'] == 1){                  //if the user is an admin
-                        echo'
+                <?php
+                //this checks to see if someone is loged in. you set the uid when you login
+                if (isset($_SESSION['uid'])):
+                    if ($_SESSION['privileged'] == 1): //if the user is an admin ?>                  
                         <li class="nav-item">
                             <a class="nav-link" href="admin.php">Admin</a>
                          </li>
-                        ';
-                    }
-                    //this checks to see if someone is loged in. you set the uid when you login
-                    echo '
-                        <li class="nav-item">
-                            <a class="nav-link" href="/index.php">Home</a>
-                        </li>
-                       <li class="nav-item">
-                            <a class="nav-link" href="/profile.php">Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/gallery.php">Gallery</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="includes/logout.php">Logout</a>
-                        </li>';
-                } else{
-                    echo '
+                    <?php endif ?>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/profile.php">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/gallery.php">Gallery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="includes/logout.php">Logout</a>
+                    </li>
+                <?php else: ?>
                     <li class="nav-item">
                         <a class="nav-link" href="/login.php">Login</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/signup.php">Signup</a>
-                    </li>';
-                }
-                ?>
-
-                <!--ldlfgjladksjgldj-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Reviews
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Replace Me</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Replace Me 2</a>
-                    </div>
-                </li>
-                -->
-
+                    </li>
+                <?php endif ?>
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
--- js/comments.js
+++ js/comments.js
@@ -46,24 +46,7 @@ document.querySelectorAll('.comment').forEach(function(elem) {
 				}			
 			}
 		});
-		
-		// just placeholder values for now
-		let reply = function() {
-			makeComment({
-				uid: genUID(),
-				author: 'placeholder_user',
-				author_image: 'data:image/svg+xml,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%20%3F%3E%0A%3Csvg%20width%3D%2240px%22%20height%3D%2240px%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20style%3D%22background-color%3A%20%23868686%22%3E%0A%3Ctext%20x%3D%2250%25%22%20y%3D%2230px%22%20style%3D%22text-anchor%3A%20middle%3B%20font-size%3A%202.1rem%3B%20font-weight%3A%20bold%22%20width%3D%22100%25%22%20height%3D%22100%25%22%20fill%3D%22%23313131%22%3E%3F%3C%2Ftext%3E%0A%3C%2Fsvg%3E',
-				author_url: 'javascript:alert("this would take you to a profile page");void 0;',
-				text: input.value,
-				rating: 0,
-				time: new Date().toLocaleTimeString([], { timeStyle: 'short' }),
-				role: 'resident',
-				parent: elem.getAttribute('data-uid'),
-				replies_permitted: elem.querySelector('#allow-replies--' + data.uid).checked
-			});
-		}
-		elem.querySelector('button').addEventListener('click', reply);
-		
+			
 		input.addEventListener('input', () => { input.style.height = ''; input.style.height = input.scrollHeight + 'px'; }); // fix textarea height
 	}
 	
