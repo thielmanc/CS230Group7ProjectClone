148e80e9d2176a4a9678d5bc950a30c6e0398ea8
dwm0013 <dwm0013@mix.wvu.edu>
2021-04-28 14:33:10 -0400

    Admin Update. formatting and bugs

--- display-reports.php
+++ display-reports.php
@@ -3,6 +3,7 @@
 require_once 'includes/display-report-helper.php';
 require_once 'includes/dbhandler.php';
 
+
 $conn = mysqli_connect($servename, $DBuname, $DBPass, $DBname);
 
 if (!$conn) {
@@ -27,29 +28,24 @@ if (mysqli_num_rows($result) > 0) {
                     <img class="mr-3" src="'.$picpath['pfpurl'].'" style="max-width: 75px; max-height: 75px; border-radius: 50%;">
                         <div class="media-body">
                             <h4 class="mt-0">'.$row['uname'].'</h4>
-                            <p>'.$row['title'].',</p>
+                            <p>'.$row['title'].'</p>
                             <p>'.$row['revdate'].'</p>
                             <p>'.$row['reviewtext'].'</p>
                         </div>
                 </div>
+                <form action="includes/display-report-helper.php" method="POST">
+                    <input type="hidden" name="id" value='.$row['revid'].'>
+
+                    <div class="mx-auto">
+                        <div class="form-group">
+                            <button class="btn btn-outline-success mx-auto" type="submit" name="approve-submit" id="approve-submit" style="width: 100%;" >Approve Comment</button>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn btn-outline-danger mx-auto" type="submit" name="remove-submit" id="remove-submit" style="width: 100%;" >Remove Comment</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-
-            <form action="includes/display-report-helper.php" method="POST">
-                <input type="hidden" name="id" value='.$row['revid'].'>
-
-                <div class="mx-auto">
-                    <div class="form-group">
-                        <button class="btn btn-outline-success mx-auto" type="submit" name="approve-submit" id="approve-submit" style="width: 100%;" >Approve Comment</button>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-outline-danger mx-auto" type="submit" name="remove-submit" id="remove-submit" style="width: 100%;" >Remove Comment</button>
-                    </div>
-                </div>
-            </form>
-
-            <hr>
-            <hr>
              ';
     }
 }
