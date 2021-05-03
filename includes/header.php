7d6e8aa08917b5740ec15b7c8da54fd4bd47ca5e
soup-tech <campbellssoup21@gmail.com>
2021-05-02 21:39:57 -0400

    More SonarQube

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php session_start() ?>
-<!DOCTYPE html lang="en">
-<html>
-<head lang="en">
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title></title>
