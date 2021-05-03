81a586dda44f428d52ab280651747c267e00aa77
kwb0008 <test@gmail.com>
2021-05-03 18:41:04 -0400

    display report/helper comments

--- display-reports.php
+++ display-reports.php
@@ -8,20 +8,23 @@ $conn = mysqli_connect($servename, $DBuname, $DBPass, $DBname);
 
 if (!$conn) {
     die("Connection failed...".mysqli_connect_error());
-    # code...
+    
 }
 
 $item = $_GET['id'];
+//Gets all reviews that have been reported (status is not 0 if reported)
 $sql = "SELECT * FROM reviews WHERE status >= 1";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) { 
+    while ($row = mysqli_fetch_assoc($result)) { //while there is a comment to show
         $uname = $row['uname'];
         $propic = "SELECT pfpurl FROM profiles WHERE uid=(SELECT uid FROM users WHERE uname='$uname');";//There is no profpic in profiles db. Along with no values for uname.
         $res = mysqli_query($conn, $propic);
         $picpath = mysqli_fetch_assoc($res);
 
+        //top half - displays a comment along with the data and the profile picture
+        //bottom half - approve (set status to 0 - normal) and remove buttons(delete the comment)
         echo '
             <div class="card mx-auto" style="width: 30%; padding: 5px; margin-bottom: 10px;">
                 <div class="media">
@@ -35,7 +38,7 @@ if (mysqli_num_rows($result) > 0) {
                 </div>
                 <form action="includes/display-report-helper.php" method="POST">
                     <input type="hidden" name="id" value='.$row['revid'].'>
-
+                    
                     <div class="mx-auto">
                         <div class="form-group">
                             <button class="btn btn-outline-success mx-auto" type="submit" name="approve-submit" id="approve-submit" style="width: 100%;" >Approve Comment</button>
--- includes/display-report-helper.php
+++ includes/display-report-helper.php
@@ -1,19 +1,22 @@
 <?php 
 require_once 'dbhandler.php';
 date_default_timezone_set('UTC');
-
+//approve button
 if (isset($_POST['approve-submit'])) {
     
     $review_ID = $_POST['id'];
+    //set the status value in the database back to normal (0)
     $sql = "UPDATE reviews SET status='0' WHERE revid=$review_ID";
     mysqli_query($conn, $sql);
 
     header("Location: ../admin.php?success=approved");
     exit();
 }
+//remove button
 if (isset($_POST['remove-submit'])) {
     
     $review_ID = $_POST['id'];
+    //deletes the comment from the database
     $sql = "DELETE FROM reviews WHERE revid = $review_ID";
     mysqli_query($conn, $sql);
 
